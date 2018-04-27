0d353683e84e64935eeffe7ffeb41bc379c8ece5
2650831723021 <2650831723021@LAB-122.lab.fatecsaoroque.edu.br>
2018-04-26 21:34:14 -0300

    arquivo com especificações dos padroes e a estrutura padrão

---
+++ site/footer.php
@@ -0,0 +1,6 @@
+        <footer></footer>
+
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
---
+++ site/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Nome - pagina</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <header></header>
