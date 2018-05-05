ab6cd9e34d568452fa3e99cdeecacdcc7419079f
2650831723019 <2650831723019@Lab-179.lab.fatecsaoroque.edu.br>
2018-05-05 12:15:51 -0300

    Alterado o arquivo dancas.php

---
+++ site/contato.php
@@ -0,0 +1,50 @@
+<?php
+include './header.php';
+?>
+        <div class="container">
+            <div class="row">
+                <div class="line"></div>
+            </div>
+            <div>
+                <h1>Mande um Feedback</h1>
+            </div>
+            <div class="row">
+                <div class="line"></div>
+            </div>
+            <form>
+                <div class="row">
+                    <label class="inpum">
+                        <!--Nome completo:-->
+                        <input type="text" name="Nome completo" class="form-control" value="" placeholder="Nome completo:"/>
+                    </label>
+                </div>
+                <div class="row">
+                    <label class="inpdois">
+                        <!--E-Mail-->
+                        <input type="text" name="E-mail" class="form-control" value="" placeholder="E-Mail"/>
+                    </label>
+                </div>
+                <div class="row">
+                    <div class="line"></div>
+                </div>
+                <div>
+                    <h2>Deixe sua Mensagem</h2>
+                </div>
+                <div class="row">
+                    <div class="line"></div>
+                </div>
+                <div class="row">
+                    <label>
+                        <!--Area de Texto-->
+                        <textarea></textarea>
+                    </label>
+                </div>
+                <div class="row">
+                    <label>
+                        <input class="send" type="submit" value="Enviar"> 
+                    </label>
+                </div>
+            </form>
+        </div>
+<?php
+include './footer.php';
