b7066d6a43ec6a75ed14591e47787d50ea162537
2650831723019 <2650831723019@Lab-179.lab.fatecsaoroque.edu.br>
2018-05-22 21:21:32 -0300

    adicionado roupas.php, alterações dancas.php, adicinado class no style.css, adicionado os links na matriz no geral.php

--- site/roupas.php
+++ site/roupas.php
@@ -1,16 +1,3 @@
-<div class="container">
-    <div class="row d-none d-lg-block">
-        <div class="col-lg-12 d-lg-block">
-            <section class="sectionImgDanca">
-                <picture>
-                    <source media="(min-width: 1200px)" srcset="midia/danca/topoDesktopDanca_1200.jpg">
-                    <img class="img-fluid" src="midia/danca/topoDesktopDanca_1200.jpg" alt="Dança tradicional japonesa, mulheres com roupa tradicional" style="width:auto;">
-                </picture>
-            </section>
-        </div>
-    </div>
-</div>
-
 <div class="container desktopTopo">
     <div class="row">
         <div class="col-lg-4">
@@ -24,27 +11,29 @@
 
         <div class="col-lg-8 col-md-12 col-xs-12">
             <article>
-                <h1 class="text-danger">Dança como arte</h1>
+                <h1 class="text-danger">Roupas</h1>
                 <p>
                     Pellentesque molestie diam lacus, ut sollicitudin mauris cursus vitae. Sed est orci, facilisis eu metus vel, maximus pretium est. Donec non gravida ligula. Fusce ac imperdiet lorem. Mauris feugiat tortor libero, non tempus nibh mattis eu. Quisque rhoncus libero bibendum sodales dapibus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer fermentum cursus pellentesque. Mauris eget elit rutrum, eleifend tortor sed, vehicula ante. Cras sed orci vel purus malesuada ultricies. Sed quis porttitor nibh. Praesent in massa tellus. In eget hendrerit massa. Mauris ut leo a purus aliquet rutrum.
-                    Sed tristique lectus commodo, scelerisque mauris a, porta odio. Vestibulum tincidunt libero id mi auctor, a elementum ligula pharetra. Ut finibus nibh a arcu suscipit, et vulputate leo semper. Fusce vulputate ipsum ac pellentesque hendrerit. Nullam pretium tincidunt mollis. Praesent nec lectus ac est commodo gravida ac ac orci. In elit arcu, porttitor in arcu et, ultrices viverra risus. Mauris a ante nibh. Praesent id sollicitudin magna, et eleifend elit. Aliquam a magna non tortor venenatis sollicitudin. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris a ante nibh. Praesent id sollicitudin magna, et eleifend elit. Aliquam a magna non tortor venenatis sollicitudin. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+                    Sed tristique lectus commodo, scelerisque mauris a, porta odio. Vestibulum tincidunt libero id mi auctor, a elementum ligula pharetra. Ut finibus nibh a arcu suscipit, et vulputate leo semper. Fusce vulputate ipsum ac pellentesque hendrerit. Nullam pretium tincidunt mollis. Praesent nec lectus ac est commodo gravida ac ac orci. In elit arcu, porttitor in arcu et, ultrices viverra risus. Mauris a ante nibh. Praesent id sollicitudin magna, et eleifend elit.
                 </p>
             </article>
 
             <div class="row">
-                <div class="col-lg-6 col-md-6 col-xs-12">
+                <div class="col-lg-12 col-md-12 col-xs-12">
                     <aside>
                         <picture>
-                            <source media="(max-width: 720px)" srcset="midia/danca/meioDesktopDanca_400.jpg">
-                            <source media="(min-width: 720px)" srcset="midia/danca/meioDesktopDanca_400.jpg">
-                            <source media="(min-width: 1200px)" srcset="midia/danca/meioDesktopDanca_400.jpg.">
-                            <img class="img-fluid" src="midia/danca/meioDesktopDanca_400.jpg" alt="duas moças moças japonesas com roupas tipicas de dança japonesa" style="width:auto;">	
+                            <source media="(max-width: 720px)" srcset="midia/danca/RoupasDanca_700.jpg">
+                            <source media="(min-width: 720px)" srcset="midia/danca/RoupasDanca_700.jpg">
+                            <source media="(min-width: 1200px)" srcset="midia/danca/RoupasDanca_700.jpg">
+                            <img class="img-fluid imgTopicoDanca" src="midia/danca/RoupasDanca_700.jpg" alt="duas moças moças japonesas com roupas tipicas de dança japonesa" style="width:auto;">	
                         </picture>
                     </aside>
                 </div>
-
-                <div class="col-lg-6 col-md-6">
-                    <aside class="d-none d-sm-block">
+            </div>
+            
+            <div class="row">
+                <div class="col-lg-12 col-md-12">
+                    <aside>
                         <p>
                             Pellentesque molestie diam lacus, ut sollicitudin mauris cursus vitae. Sed est orci, facilisis eu metus vel, maximus pretium est. Donec non gravida ligula. Fusce ac imperdiet lorem. Mauris feugiat tortor libero, non tempus nibh mattis eu. Quisque rhoncus libero bibendum sodales dapibus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer fermentum cursus pellentesque. Mauris eget elit rutrum, eleifend tortor sed, Pellentesque molestie diam lacus, ut sollicitudin mauris cursus vitae. Sed est orci, facilisis eu metus vel, maximus pretium est. Donec non gravida ligula. Fusce ac imperdiet lorem. Mauris feugiat tortor libero, non tempus nibh mattis eu. Quisque rhoncus libero bibendum sodales dapibus. Orci.
                         </p>
