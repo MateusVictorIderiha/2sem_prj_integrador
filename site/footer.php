e84e15fbeb14b904a1f03f0054b0cc3b24a1be2a
mateus <root@mateus-To-be-filled-by-O-E-M>
2018-06-03 19:20:47 -0300

    Menu terminado

--- site/footer.php
+++ site/footer.php
@@ -8,39 +8,72 @@ foreach ($infoGrupo["integrantes"] as $integrante) {
             .$integrante["nome"]."</a>";
 }
 ?>
-    <footer class="rodape border-line-top">
-        <div class="row">
-            <div class="col-12 col-lg-1">
-                <img class="simu_logo" src="midia/geral/<?= $infoGrupo["logo"]; ?>" alt="Logo <?= $infoGrupo["nome_grupo"]; ?>" />
-            </div>
-            <nav class="col-10">
-                <div class="row">
-                    <?php
-                    $itensMenu = getValuesMenu();
-                    foreach ($itensMenu as $item) {
-                         if(isset($item["subMenu"])) {
-                    ?>
-                    <ul class="col-2 d-none d-lg-block">
-                        <li class="foot_li_title"> <a href="#"></a> <?= $item["nome"]; ?> </li>
-                        <?php
-                            foreach ($item["subMenu"] as $subItem) { 
-                        ?>
-                        <li class="foot_li"> <a href="#"></a><?= $subItem["nome"]; ?></li>
-                        <?php } ?>
-                    </ul>
-                    <?php
-                         }
-                    }
-                    ?>
+        <footer class="rodape border-line-top">
+            <div class="row">
+                <div class="col-12 col-lg-1">
+                    <img class="simu_logo" src="midia/geral/<?= $infoGrupo["logo"]; ?>" alt="Logo <?= $infoGrupo["nome_grupo"]; ?>" />
                 </div>
-            </nav>
-        </div>
-        <div class="row">
-            <h3 class="direito">Copyright <?= $infoGrupo["nome_grupo"].". ".implode(", ", $integrantes)."." ?></h3>
-        </div>
-    </footer>
+                <nav class="col-10">
+                    <div class="row">
+                        <?php
+                        $itensMenu = getValuesMenu();
+                        foreach ($itensMenu as $item) {
+                             if(isset($item["subMenu"])) {
+                        ?>
+                        <ul class="col-2 d-none d-lg-block">
+                            <li class="foot_li_title"> <a href="#"></a> <?= $item["nome"]; ?> </li>
+                            <?php
+                                foreach ($item["subMenu"] as $subItem) { 
+                            ?>
+                            <li class="foot_li"> <a href="#"></a><?= $subItem["nome"]; ?></li>
+                            <?php } ?>
+                        </ul>
+                        <?php
+                             }
+                        }
+                        ?>
+                    </div>
+                </nav>
+            </div>
+            <div class="row">
+                <h3 class="direito">Copyright <?= $infoGrupo["nome_grupo"].". ".implode(", ", $integrantes)."." ?></h3>
+            </div>
+        </footer>
+
+        <div id="filter-overlay"></div>
 
         <script src="js/jquery.js"></script>
         <script src="js/bootstrap.min.js"></script>
+        
+        <script>
+            $(document).ready(function() {
+                $(".nav-item").mouseover(function () {
+                    if (window.screen.width >= 991) {
+                        var currentNav = this;
+                        $(".nav-item").each(function (i, domNavItem) {
+                            if (domNavItem !== currentNav) {
+                                $(domNavItem).removeClass("show");
+                                $(domNavItem).find(".dropdown-menu").removeClass("show");
+                                $(domNavItem).find(".nav-link.dropdown-toggle").attr("aria-expanded", false);
+                            }
+                        });
+                        $(this).find(".nav-link.dropdown-toggle").attr("aria-expanded", true);
+                    }
+                });
+                
+                $(".navbar-toggler[data-target='#navbarSupportedContent']").click(function() {
+                    var targetMenu = $(this).data("target");
+                    if(!$(targetMenu).hasClass("show")) {
+                        $("#filter-overlay").addClass("show");
+                    } else {
+                        $("#filter-overlay").removeClass("show");
+                    }
+                });
+
+                $("#filter-overlay").click(function() {
+                    $(".navbar-collapse .navbar-toggler[data-target='#navbarSupportedContent']").trigger("click");
+                });
+            });
+        </script>
     </body>
 </html>
