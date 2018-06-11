4c3ed79b0f0621accaf31fa460ae26dd177c0c0a
José Eduardo Siqueira Carlos <33236381+ZeEdu@users.noreply.github.com>
2018-06-11 19:17:38 -0300

    Teatro Clássico atualizado

--- site/cenicas.php
+++ site/cenicas.php
@@ -1,50 +1,99 @@
-<div class="container">
-    <div class="row">
-        <article class="col-12 col-md-10 col-xl-8">
-            <h1>Artes Cênicas</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet consequat tellus. Duis congue fermentum libero, vitae dignissim nisl euismod et. Donec est risus, posuere quis Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet consequat tellus. Duis congue fermentum libero.</p>
-        <picture>
-            <source media="(max-width: 760px)" srcset="midia/cenicas/300_185.jpeg">
-            <source media="(min-width: 760px)" srcset="midia/cenicas/500_310.jpeg">
-            <source media="(min-width: 1200px)" srcset="midia/cenicas/600_185.jpeg">
-            <img src="midia/cenicas/500_310.jpeg" alt="Imagem 1" style="width:auto;">
-        </picture>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet consequat tellus. Duis congue fermentum libero, vitae dignissim nisl euismod et. Donec est risus, posuere quis consequat nec, tempor in magna. Vivamus dapibus lacus ut elementum mollis. Morbi bibendum lorem ut nulla facilisis hendrerit. Cras commodo congue feugiat. Maecenas feugiat risus sit amet augue tristique malesuada euismod eu sapien. Aliquam dictum orci ante, at posuere tortor auctor eu. Etiam placerat ligula vel dolor ultrices, ac fringilla velit hendrerit.</p>
-            <h2>Surgimento do Teatro no Japão</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet consequat tellus. Duis congue fermentum libero, vitae dignissim nisl euismod et. Donec est risus, posuere quis consequat nec, tempor in magna. Vivamus dapibus lacus ut elementum mollis. Morbi bibendum lorem ut nulla facilisis hendrerit. Cras commodo congue feugiat. Maecenas feugiat risus sit amet augue tristique malesuada euismod eu sapien. Aliquam dictum orci ante, at posuere tortor auctor eu. Etiam placerat ligula vel dolor ultrices, ac fringilla velit hendrerit.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet consequat tellus. Duis congue fermentum libero, vitae dignissim nisl euismod et. Donec est risus, posuere quis consequat nec, tempor in magna. Vivamus dapibus lacus ut elementum mollis. Morbi bibendum lorem ut nulla facilisis hendrerit. Cras commodo congue feugiat. Maecenas feugiat risus sit amet augue tristique malesuada euismod eu sapien. Aliquam dictum orci ante, at posuere tortor auctor eu. Etiam placerat ligula vel dolor ultrices, ac fringilla velit hendrerit.</p>
-        </article>
+
+    <div class="container teatro_content">
+        <div class="row">
+            <article class="col-12 teatro_article">
+                <h1 class="text-danger teatro_heading">Teatro Clássico Japonês</h1>
+                    <p class="teatro_text text-justify text-sm-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet consequat tellus. Duis congue fermentum libero, vitae dignissim nisl euismod et. Donec est risus, posuere quis Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet consequat tellus. Duis congue fermentum libero.</p>
+            <picture>
+                <source media="(max-width: 760px)" srcset="midia/teatro/home/home_300.jpg">
+                <source media="(min-width: 760px)" srcset="midia/teatro/home/home_450.jpg">
+                <source media="(min-width: 1200px)" srcset="midia/teatro/home/home_600.jpg">
+                <img class="mx-auto d-block" src="midia/teatro/home/home_450.jpg" alt="Imagem 1" style="width:auto;">
+            </picture>
+                    <p class="teatro_text text-justify text-sm-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet consequat tellus. Duis congue fermentum libero, vitae dignissim nisl euismod et. Donec est risus, posuere quis consequat nec, tempor in magna. Vivamus dapibus lacus ut elementum mollis. Morbi bibendum lorem ut nulla facilisis hendrerit. Cras commodo congue feugiat. Maecenas feugiat risus sit amet augue tristique malesuada euismod eu sapien. Aliquam dictum orci ante, at posuere tortor auctor eu. Etiam placerat ligula vel dolor ultrices, ac fringilla velit hendrerit.</p>
+                <h2 class="text-danger teatro_heading">Surgimento do Teatro no Japão</h2>
+                    <p class="teatro_text text-justify text-sm-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet consequat tellus. Duis congue fermentum libero, vitae dignissim nisl euismod et. Donec est risus, posuere quis consequat nec, tempor in magna. Vivamus dapibus lacus ut elementum mollis. Morbi bibendum lorem ut nulla facilisis hendrerit. Cras commodo congue feugiat. Maecenas feugiat risus sit amet augue tristique malesuada euismod eu sapien. Aliquam dictum orci ante, at posuere tortor auctor eu. Etiam placerat ligula vel dolor ultrices, ac fringilla velit hendrerit.</p>
+                        <p class="teatro_text text-justify text-sm-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet consequat tellus. Duis congue fermentum libero, vitae dignissim nisl euismod et. Donec est risus, posuere quis consequat nec, tempor in magna. Vivamus dapibus lacus ut elementum mollis. Morbi bibendum lorem ut nulla facilisis hendrerit. Cras commodo congue feugiat. Maecenas feugiat risus sit amet augue tristique malesuada euismod eu sapien. Aliquam dictum orci ante, at posuere tortor auctor eu. Etiam placerat ligula vel dolor ultrices, ac fringilla velit hendrerit.</p>
+            </article>
+        </div>
+        <section class="row teatro_section">
+            <div class="col-12 col-md-6 col-xl-4 thumb_nail text-center">
+                <a href="http://localhost:8080/site/index.php?pagina=atores">    
+                    <div class="circular">
+                        <img class="thumb_img m-auto" src="midia/teatro/home/atores_300.jpg" alt="random img">                
+                            <div class="over_title">
+                                <h3 class="text-danger sub_cab">Atores</h3>
+                            </div>
+                            <div class="over_text">
+                                <p class="p_text">Lorem ipsum dolor sit amet, consectetur elit. Sed sit amet consequat tellus duis.</p>
+                            </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-12 col-md-6 col-xl-4 thumb_nail text-center">
+                <a href="http://localhost:8080/site/index.php?pagina=teatro_repeticao">
+                    <div class="circular">
+                        <img class="thumb_img m-auto" src="midia/teatro/home/bunraku_300.jpg" alt="random img">                
+                            <div class="over_title">
+                                <h3 class="text-danger sub_cab">Bunraku</h3>
+                            </div>
+                            <div class="over_text">
+                                <p class="p_text">Lorem ipsum dolor sit amet, consectetur elit. Sed sit amet consequat tellus duis.</p>
+                            </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-12 col-md-6 col-xl-4 thumb_nail text-center">
+                <a href="http://localhost:8080/site/index.php?pagina=teatro_repeticao">
+                    <div class="circular">
+                        <img class="thumb_img m-auto" src="midia/teatro/home/kabuki_300.jpg" alt="garoto encenando uma peça de kabuki em um festival">                
+                            <div class="over_title">
+                                <h3 class="text-danger sub_cab">Kabuki</h3>
+                            </div>
+                            <div class="over_text">
+                                <p class="p_text">Lorem ipsum dolor sit amet, consectetur elit. Sed sit amet consequat tellus duis.</p>
+                            </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-12 col-md-6 col-xl-4 thumb_nail text-center">
+                <a href="http://localhost:8080/site/index.php?pagina=teatro_repeticao">
+                    <div class="circular">
+                        <img class="thumb_img m-auto" src="midia/teatro/home/kyogen_300.jpg" alt="encenação de peça de kyogen">
+                            <div class="over_title">
+                                <h3 class="text-danger sub_cab">Kyogen</h3>
+                            </div>
+                            <div class="over_text">
+                                <p class="p_text">Lorem ipsum dolor sit amet, consectetur elit. Sed sit amet consequat tellus duis.</p>
+                            </div>
+                    </div> 
+               </a>
+            </div>
+            <div class="col-12 col-md-6 col-xl-4 thumb_nail text-center">
+                <a href="http://localhost:8080/site/index.php?pagina=teatro_repeticao">
+                    <div class="circular">
+                        <img class="thumb_img m-auto" src="midia/teatro/home/noh_300.jpg" alt="encenação de uma peça de noh">                
+                            <div class="over_title">
+                                <h3 class="text-danger sub_cab">Noh</h3>
+                            </div>
+                            <div class="over_text">
+                                <p class="p_text">Lorem ipsum dolor sit amet, consectetur elit. Sed sit amet consequat tellus duis.</p>
+                            </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col-12 col-md-6 col-xl-4 thumb_nail text-center">
+                <a href="http://localhost:8080/site/index.php?pagina=obras">
+                    <div class="circular">
+                        <img class="thumb_img m-auto" src="midia/teatro/home/pecas_300.jpg" alt="ilustração de uma peça de kabuki">                
+                            <div class="over_title">
+                                <h3 class="text-danger sub_cab">Peças e Influências</h3>
+                            </div>
+                            <div class="over_text">
+                                <p class="p_text">Lorem ipsum dolor sit amet, consectetur elit. Sed sit amet consequat tellus duis.</p>
+                            </div>
+                    </div>
+                </a>
+            </div>
+        </section>
     </div>
-    <section class="row">
-        <div class="col-12 col-md-6 col-xl-4 sec_um">
-            <h3>Notícias</h3>
-            <img src="midia/cenicas/300_185.jpeg" alt="random img">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet consequat tellus. Duis congue fermentum libero.</p>
-        </div>
-        <div class="col-12 col-md-6 col-xl-4 sec_um">
-            <h3>Bunraki</h3>
-            <img src="midia/cenicas/300_185.jpeg" alt="random img">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet consequat tellus. Duis congue fermentum libero.</p>
-        </div>
-        <div class="col-12 col-md-6 col-xl-4 sec_um">
-            <h3>Kabuki</h3>
-            <img src="midia/cenicas/300_185.jpeg" alt="random img">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet consequat tellus. Duis congue fermentum libero.</p>
-        </div>
-        <div class="col-12 col-md-6 col-xl-4 sec_um">
-            <h3>Kyogen</h3>
-            <img src="midia/cenicas/300_185.jpeg" alt="random img">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet consequat tellus. Duis congue fermentum libero.</p>
-        </div>
-        <div class="row col-12 col-md-6 col-xl-4 sec_um">
-            <h3>No</h3>
-            <img src="midia/cenicas/300_185.jpeg" alt="random img">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet consequat tellus. Duis congue fermentum libero.</p>
-        </div>                
-        <div class="row col-12 col-md-6 col-xl-4 sec_um">
-            <h3>Recomendações</h3>
-            <img src="midia/cenicas/300_185.jpeg" alt="random img">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet consequat tellus. Duis congue fermentum libero.</p>
-        </div>
-    </section>
-</div>
