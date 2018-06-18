1dd7581c076cfb5786f1e7bd379ff578b30bd356
mateus <2650831723021@LAB-23.lab.fatecsaoroque.edu.br>
2018-06-18 01:07:12 -0300

    quem somos

--- site/quem_somos.php
+++ site/quem_somos.php
@@ -13,24 +13,30 @@ $dadosGrupo = getInfoGrupo();
             </figure>
         </div>
         <div class="col-md-12">
-            <p><?= $dadosGrupo["descricao"]; ?></p>
+            <p class="text-justify"><?= $dadosGrupo["descricao"]; ?></p>
         </div>
     </div>
     <?php foreach ($dadosGrupo["integrantes"] as $integrante) { ?>
     <div class="row integrante">
-        <section class="col-lg-2 col-md-4 col-12 img">
+        <section class="col-lg-2 col-md-4 col-12 img mb-3 mb-sm-0 text-center">
             <?= returnHtmlImg("geral", $integrante["imagem"], "", "mw-100 rounded-circle"); ?>
         </section>
-        <ul class="col-lg-10 col-md-8 col-12">        
+        <ul class="col-lg-10 col-md-8 col-12">
+            <li><strong>Nome:</strong> <?= $integrante["nome"]." ".$integrante["sobre_nome"]; ?></li>
+            <li><strong>RA:</strong> <?= $integrante["ra"]; ?></li>
             <li><strong>Funções:</strong> <?= $integrante["funcoes"]; ?></li>
             <li>
                 <strong>Áreas responsável:</strong> 
-                <?php foreach ($integrante["links"] as $link) { ?>
-                <a href="index.php?page=<?= $link["page"]; ?>" title="<?= $link["title"] ?>"><?= $link["nome"]; ?></a> <?= $link["descricao"] ?? ""; ?>
-                <?php } ?>
+                <?php
+                foreach ($integrante["links"] as $link) { 
+                    if (!empty($link["nome"])) {
+                ?>
+                <a href="index.php?pagina=<?= $link["page"]; ?>" title="<?= $link["title"] ?>"><?= $link["nome"]; ?></a><?= end($integrante["links"]) != $link ? ", " : ""; ?>
+                <?php 
+                    } 
+                } 
+                ?>
             </li>
-            <li><strong>Nome:</strong> <?= $integrante["nome"]." ".$integrante["sobre_nome"]; ?></li>
-            <li><strong>RA:</strong> <?= $integrante["ra"]; ?></li>
             <li><strong>Mini biografia:</strong> <?= substr($integrante["bibliografia"], 0, 144); ?></li>
         </ul>
     </div>
