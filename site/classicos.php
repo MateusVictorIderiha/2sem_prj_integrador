078fb18abcaf225ce9eebe450d6f9a05f0a3d9db
José Eduardo Siqueira Carlos <33236381+ZeEdu@users.noreply.github.com>
2018-06-23 01:56:15 -0300

    Seção de Teatro Concluida

--- site/classicos.php
+++ site/classicos.php
@@ -1,6 +1,5 @@
-<?php
-    $pecas = pecasContent();
-    $cinema = cinemaContent();
+<?php  
+    $classicos = classicosInterna();
 ?>    
 <div class="container teatro_content">
         <h1 class="titulo text-center teatro_titulo_pos">Obras e Influenciados</h1>
@@ -9,27 +8,32 @@
 		    <div class="line line_obras"></div>
 			<h2 class="obras_sub_heading col-12">Peças Clássicas</h2>
 		<?php 
-		    foreach ($pecas as $dataPecas) {
+		    foreach ($classicos as $dataPecas) {
+			if($dataPecas["formato"] == "peca" ) {
 		?>
 		    <section class="col-12 col-sm-12 col-md-6 col-xl-3">
-			<img class="pecas_img col-12" src="midia/teatro/<?= $dataPecas['imageSource'] ?>.jpg" title="<?= $dataPecas['credito'] ?>" alt="<?= $dataPecas['alt'] ?>" width="300" height="185">
+			<img class="pecas_img col-12" src="midia/teatro/<?= $dataPecas['imagem'] ?>.jpg" title="<?= $dataPecas['credito'] ?>" alt="<?= $dataPecas['alt'] ?>" width="300" height="185">
 			<a class="obra_nome col-12" href="index.php?pagina=classicos_internas&id=<?= $dataPecas['id'] ?>"><?= $dataPecas['titulo'] ?></a>                        
 		    </section>
 		<?php
-		    }
+		    
+			}
+		}
 		?>
             </div>
             <div class="row text-center">
                 <div class="line line_obras"></div>
 		    <h2 class="obras_sub_heading col-12">Obras Influenciadas</h2>
 		<?php 
-		    foreach ($cinema as $dataCinema) {
+		    foreach ($classicos as $dataCinema) {
+			if ($dataCinema["formato"] == "filme" ) {
 		?>
 		    <section class="col-12 col-sm-12 col-md-6 col-xl-3">
-			<img class="pecas_img col-12" src="midia/teatro/<?= $dataCinema['imageSource'] ?>.jpg" alt="<?= $dataCinema['alt'] ?>" width="300" height="185">
+			<img class="pecas_img col-12" src="midia/teatro/<?= $dataCinema['imagem'] ?>.jpg" alt="<?= $dataCinema['alt'] ?>" width="300" height="185">
                         <a class="obra_nome col-12" href="index.php?pagina=classicos_internas&id=<?= $dataCinema['id'] ?>"><?= $dataCinema['titulo'] ?></a>                        
                     </section>
 		<?php
+			}
 		    }
 		?>
             </div>            
