0b40fb291b021b91078678325efb1c279be37f70
mateus <mateus@mateus-To-be-filled-by-O-E-M>
2018-06-24 09:18:51 -0300

    Adicionado atualizações rafael

--- site/aprenda_a_fazer.php
+++ site/aprenda_a_fazer.php
@@ -3,10 +3,12 @@
         <section class="row">
             <div class="col-md-10">
                 <h1 class="text-center">Aprenda a fazer o seu :</h1>
-                <h3 class="text-left">Origami Tsuru</h3>
-                <div class="m-auto"><iframe class="" width="560" height="315" src="https://www.youtube.com/embed/pzS0ToWZ9DA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis</p>
+                <h3 class="text-left">Origami Borboletas</h3>
+                <p class="text-center"> <iframe width="560" height="315" src="https://www.youtube.com/embed/kKj0nOhywWE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></p>
+                <p class="pl-5 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis
+                    Lorem ipsum dolor sit Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis
+                    Lorem ipsum dolor sit Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis
+                    Lorem ipsum dolor sit  amet, consectetur adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis</p>
             </div>
         </section>
 
@@ -14,22 +16,22 @@
 
         <section class="row">
             <div class="col-md-3">
-                <h3>Borboletas:</h3>
+               <a href="index.php?pagina=aprenda_a_fazer"> <h3>Borboletas:</h3>
 
-                <img class="img-rounded img-responsive center-block" style="height: 140px; width: 250px;" src="midia/origame/AprendaFazer1.jpg" alt="img">
-            </div>
+                <img class="img-rounded img-responsive imgHome center-block" style="height: 140px; width: 250px;" src="midia/origame/AprendaFazer1.jpg" alt="img">
+               </a> </div>
             <div class="col-md-3">
                 <h2>Tipo de papel utilizado</h2>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis</p>
+                <p class="pt-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis</p>
             </div>
             <div class="col-md-3">
-                <h3>Veja o Tsuru pronto:</h3>
-                <img class="img-rounded img-responsive center-block" style="height: 140px; width: 250px;" src="midia/origame/AprendaFazer2.jpg" alt="img">
-            </div>
+                <a href="index.php?pagina=origame"> <h3>Veja o Tsuru pronto:</h3>
+                <img class="img-rounded img-responsive center-block imgHome" style="height: 140px; width: 250px;" src="midia/origame/AprendaFazer2.jpg" alt="img">
+            </a></div>
             <div class="col-md-3">
                 <h2>Tipo de papel utilizado</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis</p>
+                <p class="pt-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis</p>
 
         </section>
     </article> 
--- site/historia_origame.php
+++ site/historia_origame.php
@@ -1,10 +1,10 @@
 <div class="countanier">
     <article class="row">
-        <div class="col-md-6" class="text-center">
+        <div class="col-md-4 col-12" class="text-center">
             <h1 class="text-center">Historia do Origami</h1>
-            <img  class="img-rounded img-responsive center-block" style="height: 860px; width: 540px; padiwng" src="midia/origame/origameHome1.jpg" alt="img">
+            <img class="imgHome"  style="width: 300px;" class="img-rounded img-responsive" style="height: 860px; width: 540px; padiwng" src="midia/origame/origameHome1.jpg" alt="img">
         </div>
-        <div class="col-md-4" class="text-center"> 
+        <div class="col-md-6 col-12" class="text-center"> 
             <h2 class="text-center"> Inicio </h2>
             <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis distinctio dignissimos eos. Aut autem aliquid r adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis distinctio dignissimos eos. Aut autem aliquid ma maxime mollitia, quam illo.</p>
             <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis distinctio dignissimos eos. Aut autem aliquid r adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis distinctio dignissimos eos. Aut autem aliquid ma maxime mollitia, quam illo.</p>
@@ -14,46 +14,48 @@
 
         </div>
     </article>
-    <h2 class="text-center">Curiosidades</h2>
+   
+    <a href="index.php?pagina=curiosidades_origame"> <h2 class="text-center">Curiosidades</h2></a>
         <aside class="row">
-            <div class="col-md-2">
-                <img class="img-rounded img-responsive center-block  mw-100" style="height: 140px; width: 400px;" src="midia/origame/origameHome2.jpg" alt="img">
+            <div class="col-md-3 col-12">
+                <a href="index.php?pagina=origame"><img class="img-rounded img-responsive center-block imgHome  mw-100" style="height: 160px; width: 280px;" src="midia/origame/origameHome2.jpg" alt="img">
             </div>
-            <div class="col-md-3">
-                <h3 class="text-align-center" class="">Origami 1</h3>
+            <div class="col-md-3 col-12">
+                <h3 class="text-align-center" class="">Origami Tsuru</h3>
+                </a><p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis distinctio dignissimos</p>
                 <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis distinctio dignissimos</p>
-                <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis distinctio dignissimos</p>
             </div>
-            <div class="col-md-2">
-                <img class="img-rounded img-responsive center-block mw-100" style="height: 140px; width: 250px;" src="midia/origame/OrigameHome3.jpg" alt="img">
+            <div class="col-md-3 col-12">
+                <a href="index.php?pagina=origame2"><img class="img-rounded img-responsive center-block imgHome mw-100" style="height: 140px; width: 250px;" src="midia/origame/OrigameHome3.jpg" alt="img">
             </div>
-            <div class="col-md-3">
-                <h3>Origami 2</h3>
+            <div class="col-md-3 col-12">
+                <h3>Origami: Dragão</h3>
+                </a>
                 <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis distinctio dignissimos</p>
                 <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis distinctio dignissimos</p>
             </div>
         </aside>
     <h2 class="text-center">Topicos</h2>
         <aside class="row">
-            <div class="col-md-3">
-                <h3 class="text-center">Aprenda a fazer o Tsuru </h3>
-                <img class="img-rounded img-responsive center-block" style="height: 140px; width: 250px;" src="midia/origame/Bandeira.jpg" alt="img">
-                <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis distinctio dignissimos</p>
+            <div class="col-md-3 col-12">
+                 <a href="index.php?pagina=aprenda_a_fazer2"> <h3 class="text-center">Aprenda a fazer o Tsuru </h3>
+                <img class="img-rounded img-responsive center-block imgHome" style="height: 140px; width: 250px;" src="midia/origame/HomeCuriosidades1.jpg" alt="img">
+                 </a><p class="text-justify ml-20">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis distinctio dignissimos</p>
             </div>
-            <div class="col-md-3">
-                <h3 class="text-center"> Modelo pronto  </h3>
-                <img class="img-rounded img-responsive center-block" style="height: 140px; width: 250px;" src="midia/origame/Bandeira.jpg" alt="img">
-                <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis distinctio dignissimos</p>
+            <div class="col-md-3 col-12">
+                <a href="index.php?pagina=origame2">  <h3 class="text-center"> Modelo pronto  </h3>
+                <img class="img-rounded img-responsive center-block imgHome" style="height: 140px; width: 250px;" src="midia/origame/modeloprontohome.jpg" alt="img">
+                </a><p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis distinctio dignissimos</p>
             </div>
-            <div class="col-md-3">
-                <h3 class="text-center"> Aprenda a fazer as Borboletas </h3>
-                <img class="img-rounded img-responsive center-block" style="height: 140px; width: 250px;" src="midia/origame/Bandeira.jpg" alt="img">
-                <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis distinctio dignissimos</p>
+            <div class="col-md-3 col-12">
+                 <a href="index.php?pagina=aprenda_a_fazer"> <h3 class="text-center"> Aprenda a fazer as Borboletas </h3>
+                <img class="img-rounded img-responsive center-block imgHome" style="height: 140px; width: 250px;" src="midia/origame/aprendaafazerborboletashome.jpg" alt="img">
+                </a><p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis distinctio dignissimos</p>
             </div>
-            <div class="col-md-3">
-                <h3 class="text-center"> Modelo pronto</h3>
-                <img class="img-rounded img-responsive center-block" style="height: 140px; width: 250px;" src="midia/origame/Bandeira.jpg" alt="img">
-                <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis distinctio dignissimos</p>
+            <div class="col-md-3 col-12">
+               <a href="index.php?pagina=origame"> <h3 class="text-center"> Modelo pronto</h3>
+                <img class="img-rounded img-responsive center-block imgHome" style="height: 140px; width: 250px;" src="midia/origame/vermelho.jpg" alt="img">
+                </a><p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis distinctio dignissimos</p>
             </div>
         </aside>
 </div>
