0ac7b0f28014574bb6f1517bdc310566e02c42fb
eduardo <eduardo@acer>
2018-10-17 19:43:59 -0300

    Link do menu mobile corrigido, thumbnails seção teatro corrigidas.

---
+++ site/aprenda_a_fazer.php
@@ -0,0 +1,39 @@
+<div class="container">
+    <article class="row">
+	<div class="col-md-12 col-12">
+	    <h1 class="text-center titulo">Aprenda a fazer o seu :</h1>
+	    <h2 class="text-left">Origami Borboletas</h2>
+	    <p class="text-center"> <iframe width="560" height="315" src="https://www.youtube.com/embed/kKj0nOhywWE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></p>
+	    <p class="pl-5 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis
+		Lorem ipsum dolor sit Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis
+		Lorem ipsum dolor sit Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis
+		Lorem ipsum dolor sit  amet, consectetur adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis</p>
+	</div>
+    </article>
+ <hr class="hrOrigami">
+	    <div class="row">
+		<section class="row col-12 col-md-6">
+		    <div class="col-12 col-md-6">
+			<a href="index.php?pagina=aprenda_a_fazer"> <h3>Borboletas:</h3>
+			    <img class="img-rounded img-responsive imgHome center-block" style="height: 140px; width: 250px;" src="midia/origame/AprendaFazer1.jpg" alt="Origami feito em papel colorido representando borboletas">
+			</a> 
+		    </div>
+		    <div class="col-12 col-md-6">
+			<h4>Tipo de papel utilizado</h4>
+			<p class="pt-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis</p>
+		    </div>
+		</section>
+		<section class="row col-12 col-md-6">
+		    <div class="col-12 col-md-6">
+			<a href="index.php?pagina=origami"> <h3>Veja o Tsuru pronto:</h3>
+			    <img class="img-rounded img-responsive center-block imgHome" style="height: 140px; width: 250px;" src="midia/origame/AprendaFazer2.jpg" alt="Origami feito em papel colorido representando um passaro">
+			</a>
+		    </div>
+		    <div class="col-12 col-md-6">
+			<h4>Tipo de papel utilizado</h4>
+			<p class="pt-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto eius libero accusamus corporis, earum corrupti, quaerat quasi molestiae hic, nobis</p>
+		    </div>
+		</section>
+	    </div>
+        
+</div>
